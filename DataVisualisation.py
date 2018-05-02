103f74799499fd4555a41e5c236e03ab3b5b9429
acharlet_laptop <arthur.charlet@ens-lyon.fr>
2018-05-02 10:59:15 +0200

    code fonctionnel

--- DataVisualisation.py
+++ DataVisualisation.py
@@ -1,44 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-Ncell=20; Nvar=3
+Ncell=1000; Nvar=3
 dx = 1./Ncell
 x = np.arange(0,1,dx)
 k=0
-path = '/media/acharlet/Data/Arthur/Documents/Cours/4A CRAL/Code/Shared/Hydro/Resultats'
+path = '/media/acharlet/Data/Arthur/Documents/Cours/4A_CRAL/Code/Perso/Hydro/Resultats_Nc1000_dt0.001_1t1s'
 os.chdir(path)
 maxrho=minrho=maxP=minP=maxv=minv=0.
-j=0
-for filename in os.listdir(path):
-    if filename.endswith(".dat"):
-        j+=1
-        if j>50:
-            break
-        with open(filename) as f:
-            rho=[]
-            P=[]
-            v=[]
-            for line in f:
-                l=line.split()
-                l=[float(i) for i in l]
-                rho.append(l[0])
-                P.append(l[1])
-                v.append(l[2])
-                maxrho=max(maxrho,np.amax(rho))
-                minrho=min(minrho,np.amin(rho))
-                maxP=max(maxP,np.amax(P))
-                minP=min(minP,np.amin(P))
-                maxv=max(maxv,np.amax(v))
-                minv=min(minv,np.amin(v))
+#for filename in os.listdir(path):
+#    if os.path.isfile(os.path.join(path,filename)) and 'data' in filename:
+#        with open(filename) as f:
+#            rho=[]
+#            P=[]
+#            v=[]
+#            for line in f:
+#                l=line.split()
+#                l=[float(i) for i in l]
+#                rho.append(l[0])
+#                P.append(l[1])
+#                v.append(l[2])
+#                maxrho=max(maxrho,np.amax(rho))
+#                minrho=min(minrho,np.amin(rho))
+#                maxP=max(maxP,np.amax(P))
+#                minP=min(minP,np.amin(P))
+#                maxv=max(maxv,np.amax(v))
+#                minv=min(minv,np.amin(v))
+#max1=max(maxP,maxrho)
+#min1=min(minP,minrho)
 
-max1=max(maxP,maxrho)
-min1=min(minP,minrho)
-j=0
-for filename in os.listdir(path):
-    if filename.endswith(".dat"):
-        j+=1
-        if j>50:
-            break
+for filename in sorted(os.listdir(path)):
+    if os.path.isfile(os.path.join(path,filename)) and 'data' in filename:
         with open(filename) as f:
             rho=[]
             P=[]
@@ -47,19 +39,23 @@ for filename in os.listdir(path):
                 l=line.split()
                 l=[float(i) for i in l]
                 rho.append(l[0])
-                P.append(l[1])
-                v.append(l[2])
+                P.append(l[2])
+                v.append(l[1])
             fig,ax1 = plt.subplots()
-            ax1.plot(x,rho,'r:',label='density')
-            ax1.plot(x,P,'b:',label='pressure')
+            ax1.plot(x,rho,'r:',label='Density')
+            ax1.plot(x,P,'b:',label='Pressure')
             ax1.set_xlabel('x')
-            ax1.set_ylabel('rho, P')
-            ax1.set_ylim(-5,15)
+            ax1.set_ylabel('rho, P',rotation=0)
+            ax1.set_xlim(0,1)
+            ax1.set_ylim(0,5)
             ax2=ax1.twinx()
-            ax2.plot(x,v,'g:',label='speed')
-            ax2.set_ylabel('v',color='g')
+            ax2.plot(x,v,'g:',label='Speed')
+            ax2.set_ylabel('v',color='g',rotation=0)
             ax2.tick_params('y',colors='g')
-            ax2.set_ylim(1.2*minv,1.2*maxv)
+            ax2.set_ylim(-0.2,0.5)
+            lines, labels = ax1.get_legend_handles_labels()
+            lines2, labels2 = ax2.get_legend_handles_labels()
+            ax2.legend(lines + lines2, labels + labels2)
             plt.savefig('Step{0}.png'.format(k))
             plt.close()
             k+=1
--- Hydro_v1.f90
+++ Hydro_v1.f90
@@ -1,12 +1,14 @@
 module grid
   implicit none
-  ! creates grid and keeps it in memory for use
-  integer,parameter :: Ncell=20,Ndim=1,Nvar=3,Nsteps=200
-  real(kind=8),parameter :: Xmin=0.d0,Xmax=1.d0,dx=(Xmax-Xmin)/Ncell,dt_i=0.01d0,T_tot=Nsteps*dt_i
+  ! creates grid
+  integer,parameter :: Ncell=1000,Ndim=1,Nvar=3,Nsteps=1000
+  real(kind=8),parameter :: dx=0.01d0,dt_i=0.001d0,T_tot=Nsteps*dt_i
   real(kind=8),save :: c,dt,t
   real(kind=8),dimension(1:Nvar,1:Ncell),save :: primVar,consVar
-  character(len=16),save :: filename
-  character(len=64),save :: path='/home/acharlet/4A CRAL/Code/Shared/Hydro/Resultats/'
+   real(kind=8),dimension(1:Nvar,1:Ncell+1),save :: gridFlux,gridDiss,consR,consL
+  character(len=16),save :: filename1,filename2,filename3
+  character(len=128),save :: path='/media/acharlet/Data/Arthur/Documents/Cours/4A_CRAL/Code/Perso/Hydro/Resultats/',FileName
+  
   contains
     subroutine init()
       integer :: i
@@ -19,130 +21,208 @@ module grid
       consVar(2,:) = 0.d0          ! S = rho*h*v , v=0
       consVar(3,:) = 4.d0*primVar(1,:) ! U = rho*h-p , h=5, rho=p
 
-      write (filename, "('data0000.dat')")
-      open(10,file=path//filename,status='new')
+      write (filename1, "('data0000.dat')")
+      FileName=trim(adjustl(path))//trim(adjustl(filename1))
+      open(10,file=FileName,status='new')
 100   format (3e20.10)
       do i=1,Ncell
+         !write(10,100) consVar(1,i), consVar(2,i), consVar(3,i)
          write(10,100) primVar(1,i), primVar(2,i), primVar(3,i)
       end do
       close(10)
     end subroutine init
 
- ! equations d'evolution
-    function timeDer(V,N) result(dV)
-      real(kind=8),dimension(Nvar) :: V_N,V_Nm,V_Np,dV! V = (D,S,U), m indice-1, p indice+1
-      real(kind=8),dimension(Nvar),intent(in) :: V
-      real(kind=8) :: h,hm,hp,dD,dU,dS,dW
-      integer,intent(in) :: N
+  ! variables primitives vers variables conservatives
+  function cons2prim(Vc) result(Vp)
+    real(kind=8),dimension(Nvar),intent(in) :: Vc
+    real(kind=8),dimension(Nvar) :: Vp
+    Vp(1) = Vc(1)
+    Vp(3) = (Vc(3)-Vc(1))/3.d0
+    Vp(2) = Vc(2)/(Vc(1)+4.d0*Vp(3))
+  end function cons2prim
 
-      V_N = V
-      ! outflow conditions
-      if (N==1) then
-         V_Nm=V
-         V_Np= consVar(:,N+1)
-      elseif (N==Ncell) then
-          V_Nm= consVar(:,N-1)
-          V_Np= V
-      else
-         V_Nm= consVar(:,N-1)
-         V_Np= consVar(:,N+1)
-      end if
-    
-      h  = 0.2*(1.d0+4.d0*(V_N(3)/V_N(1)))
-      hp = 0.2*(1.d0+4.d0*(V_Np(3)/V_Np(1)))
-      hm = 0.2*(1.d0+4.d0*(V_Nm(3)/V_Nm(1)))
-      dW = ((V_Np(2)**2/(V_Np(1)*hp)+V_Np(3)-V_Np(1)*hp)-(V_Nm(2)**2/(V_Nm(1)*hm)+V_Nm(3)-V_Nm(1)*hm))/(2.d0*dx)
-      dD = (V_Np(2)/hp-V_Nm(2)/hm)/(2.d0*dx)
-      dU = (V_Np(3)-V_Nm(3))/(2.d0*dx)
-      dS = (V_Np(2)-V_Nm(2))/(2.d0*dx)
-    
-      dV(1) = -dD
-      dV(2) = -dW
-      dV(3) = -dS
-    end function timeDer
-      
+  ! variables conservatives vers variables primitives
+  function prim2cons(Vp) result(Vc)
+    real(kind=8),dimension(Nvar),intent(in) :: Vp
+    real(kind=8),dimension(Nvar) :: Vc
+    Vc(1) = Vp(1)
+    Vc(2) = Vp(2)*(Vp(1)+4*Vp(1))
+    Vc(3) = Vp(1)+3*Vp(3)
+  end function prim2cons
+  
 end module grid
 
 program simulation
   use grid
   implicit none
-  integer :: i,j
-  !real(dp),parameter :: gam=4/3        ! isentropic expansion factor gamma
-  
+  integer :: iStep,i,l  
   call init()
-  i=1
-  do while(t<=T_tot)
-     print*, "Step ",i,", t=",t," completion: ",100*t/T_tot,"%"
-     write (filename, "('data',I4.4,'.dat')") i
-     open(10,file=path//filename,status='new')
-     call evolTVDLF
-     call cons2prim
-100  format (3e20.10e3) 
-     do j=1,Ncell
-        write(10,100) primVar(1,j), primVar(2,j), primVar(3,j)
+  do iStep=1,Nsteps
+     !print*, "Step ",iStep,", t=",t," completion: ",100*t/T_tot,"%"
+     write (filename1, "('data',I4.4,'.dat')") iStep
+     FileName=trim(adjustl(path))//trim(adjustl(filename1))
+     open(10,file=FileName,status='new')
+     call evol2t2s
+     do l = 1,Ncell
+        primVar(:,l)=cons2prim(consVar(:,l))
      end do
-     i = i+1    
+100  format (3e20.10e3) 
+     do i=1,Ncell
+        write(10,100) primVar(1,i), primVar(2,i), primVar(3,i)
+        !write(10,100) consVar(1,i), consVar(2,i), consVar(3,i)
+     end do
+     
+     !write (filename2, "('flux',I3.3,'.dat')") iStep
+     !FileName=trim(adjustl(path))//trim(adjustl(filename2))
+     !open(11,file=FileName,status='new')  
+     !do i=1,Ncell+1
+     !   write(11,100) gridFlux(1,i), gridFlux(2,i), gridFlux(3,i)
+     !end do
+     !write (filename3, "('diss',I3.3,'.dat')") iStep
+     !FileName=trim(adjustl(path))//trim(adjustl(filename3))
+     !open(12,file=FileName,status='new')
+     !do i=1,Ncell+1 
+     !   write(12,100) gridDiss(1,i),  gridDiss(2,i), gridDiss(3,i)
+     !end do
   end do
   
 contains
-! Runge Kutta 4
-  function RK4(V_i,N) result(V_f)
-    integer,intent(in) :: N
-    real(kind=8),dimension(Nvar),intent(in) :: V_i
-    real(kind=8),dimension(Nvar) :: k1,k2,k3,k4,V_f
-    k1 = timeDer(V_i,N)
-    k2 = timeDer(V_i+0.5d0*dt*k1,N)
-    k3 = timeDer(V_i+0.5d0*dt*k2,N)
-    k4 = timeDer(V_i+dt*k3,N)
-    V_f = V_i + (dt/6.d0)*(k1+2.d0*k2+2.d0*k3+k4)
-  end function RK4
-
-! Finite Volume
-  function fV1D(V_i,N) result(V_f)
-    integer,intent(in) :: N
-    real(kind=8),dimension(Nvar),intent(in) :: V_i
-    real(kind=8),dimension(Nvar) :: V_f
-    V_f = V_i - dt*timeDer(V_i,N)
-  end function fV1D
-
 ! calcul des flux
-  function flux(V) result(F)
+  function flux(Vc) result(F)
     ! flux > 0 quand va vers i croissants
-    real(kind=8) :: h
-    real(kind=8),dimension(Nvar),intent(in) :: V
-    real(kind=8),dimension(Nvar) :: F
-    h =  0.2*(1.d0+4.d0*(V(3)/V(1)))
-    F(1) = V(2)/h
-    F(2) = V(2)**2/(h*V(1))+V(3)-h*V(1)
-    F(3) = V(2)
+    real(kind=8),dimension(Nvar),intent(in) :: Vc
+    real(kind=8),dimension(Nvar) ::Vp,F
+    Vp = cons2prim(Vc)
+    F(1) = Vc(1)*Vp(2)
+    F(2) = Vc(2)*Vp(2)+Vp(3)
+    F(3) = Vp(2)*(Vc(3)+Vp(3))
   end function flux  
 
-! variables primitives vers variables conservatives
-  subroutine cons2prim
-    real(kind=8) :: h
-    integer :: i
-    do i=1,Ncell
-       h=0.2d0*(1.d0+4.d0*(consVar(3,i)/consVar(1,i)))
-       primVar(1,i) = consVar(1,i)
-       primVar(2,i) = consVar(2,i)/(h*consVar(1,i))
-       primVar(3,i) = consVar(1,i)*h-consVar(3,i)
-    end do
-  end subroutine cons2prim
-
-  subroutine evolTVDLF
-    integer :: i,j,k
+  subroutine evol1t1s
+    integer :: i,j,k,l
     real(kind=8) :: ctemp
-    real(kind=8),dimension(Nvar) :: Dm,Dp,V,Vm,Vp,Vnew
-    real(kind=8),dimension(1:Nvar,1:Ncell) :: dV,Vh
-    real(kind=8),dimension(1:Nvar,1:Ncell+1) :: F,Vl,Vr
-    
+    real(kind=8),dimension(Nvar) :: V,Vnew
+    real(kind=8),dimension(1:Nvar,1:Ncell) :: dV
+    real(kind=8),dimension(1:Nvar,1:Ncell+1) :: F,diss
     ! vérification du pas de temps
     do i = 1,Ncell
-       ctemp = sqrt(primVar(3,i)/primVar(1,i))
+       ctemp = sqrt((4.d0/3.d0)*(primVar(3,i)/primVar(1,i)))+abs(primVar(2,i))
        if (ctemp>c) c=ctemp
     end do
-    if (dt > dx/c) dt=0.8*dx/c
-    if (dt < 0.8*dx/c) dt=0.8*dx/c
+    if (dt > 0.4*dx/c) dt=0.1*dt
+    if (dt < 0.05*dx/c) dt=10*dt
+    
+    ! calcul flux aux interfaces
+    do k = 2,Ncell
+       F(:,k) = 0.5d0*(flux(consVar(:,k-1))+flux(consVar(:,k)))
+    end do
+    F(:,1) = F(:,2)
+    F(:,Ncell+1) = F(:,Ncell)
+    !gridFlux=F
+
+    ! calcul terme dissipatif
+    do k = 2,Ncell+1
+       diss(:,k) = (c+abs(primVar(2,k)))*0.5d0*(consVar(:,k-1)-consVar(:,k))
+    end do
+    diss(:,1) = diss(:,2)
+    diss(:,Ncell+1) = diss(:,Ncell)
+
+    ! calcul nouvelle valeur
+    do i=1,Ncell
+       do l=1,Nvar
+          Vnew(l) = consVar(l,i) - (dt/dx)*(F(l,i+1)-F(l,i)) - (dt/dx)*(diss(l,i+1)-diss(l,i))
+          consVar(l,i) = Vnew(l)
+       end do
+    end do
+    t=t+dt
+  end subroutine evol1t1s
+
+  subroutine evol1t2s
+   integer :: i,j,k,l
+    real(kind=8) :: ctemp
+    real(kind=8),dimension(Nvar) :: Vnew,Vm,Vp,Dm,Dp
+    real(kind=8),dimension(1:Nvar,1:Ncell) :: dV,dVc
+    real(kind=8),dimension(1:Nvar,1:Ncell+1) :: F,diss,Vl,Vr
+    ! vérification du pas de temps
+    do i = 1,Ncell
+       ctemp = sqrt((4.d0/3.d0)*(primVar(3,i)/primVar(1,i)))+abs(primVar(2,i))
+       if (ctemp>c) c=ctemp
+    end do
+    if (dt > 0.4*dx/c) dt=0.1*dt
+    if (dt < 0.05*dx/c) dt=10*dt
+
+    ! calcul limiteur minmod
+    do i=1,Ncell
+       if (i==1) then
+          Vm = primVar(:,1)
+          Vp = primVar(:,2)
+       else if (i==Ncell) then
+          Vm = primVar(:,Ncell-1)
+          Vp = primVar(:,Ncell)
+       else
+          Vm = primVar(:,i-1)
+          Vp = primVar(:,i+1)
+       end if
+       Dm = primvar(:,i) - Vm
+       Dp = Vp - primVar(:,i)
+       do j=1,Nvar
+          if (Dm(j)>=0.d0 .and. Dp(j)>=0.d0) then
+             dV(j,i) = min(Dm(j),Dp(j))
+          else if (Dm(j)<=0.d0 .and. Dp(j)<=0.d0) then
+             dV(j,i) = -min(-Dm(j),-Dp(j))
+          else
+             dV(i,j) = 0.d0
+          end if
+       end do
+    end do
+    do i=1,Ncell
+       dVc(:,i) = prim2cons(dV(:,i))
+    end do
+    
+    ! calcul valeur variables aux interfaces, k numéro interface entre i-1 et i
+    do k=2,Ncell+1
+       Vl(:,k) = consVar(:,k-1)+dVc(:,k-1)
+    end do
+    Vl(:,1) = Vl(:,2)
+    do k=1,Ncell
+       Vr(:,k) = consVar(:,k)-dVc(:,k)
+    end do
+    Vr(:,Ncell+1) = Vr(:,Ncell)
+
+    ! calcul flux aux interfaces
+    do k = 1,Ncell+1
+       F(:,k) = 0.5d0*(flux(Vr(:,k))+flux(Vl(:,k)))
+    end do
+     
+    ! calcul terme dissipatif
+    do k = 1,Ncell
+       diss(:,k) = (c+abs(primVar(2,k)))*0.5d0*(Vl(:,k)-Vr(:,k))
+    end do
+    diss(:,Ncell+1) = diss(:,Ncell)
+     
+    ! calcul nouvelle valeur
+    do i=1,Ncell
+       do l=1,Nvar
+          Vnew(l) = consVar(l,i) - (dt/dx)*(F(l,i+1)-F(l,i)) - (dt/dx)*(diss(l,i+1)-diss(l,i))
+          consVar(l,i) = Vnew(l)
+       end do
+    end do
+    t=t+dt
+  end subroutine evol1t2s
+
+  subroutine evol2t1s
+    integer :: i,j,k,l
+    real(kind=8) :: ctemp
+    real(kind=8),dimension(Nvar) :: V,Vnew,Dm,Dp,Vm,Vp
+    real(kind=8),dimension(1:Nvar,1:Ncell) :: dV,Vh
+    real(kind=8),dimension(1:Nvar,1:Ncell+1) :: F,diss
+    ! vérification du pas de temps
+    do i = 1,Ncell
+       ctemp = sqrt((4.d0/3.d0)*(primVar(3,i)/primVar(1,i)))+abs(primVar(2,i))
+       if (ctemp>c) c=ctemp
+    end do
+    if (dt > 0.4*dx/c) dt=0.1*dt
+    if (dt < 0.05*dx/c) dt=10*dt
     
     ! calcul limiteur minmod
     do i=1,Ncell
@@ -172,49 +252,112 @@ contains
     
      ! calcul "demi pas de temps"
      do i = 1,Ncell
-        Vh(:,i) = V - dt/(2*dx)*(flux(V+0.5*dV(:,i))-(V-0.5*dV(:,i)))
+        Vh(:,i) = consVar(:,i) - dt/(2*dx)*(flux(consVar(:,i)+0.5*dV(:,i))-flux(consVar(:,i)-0.5*dV(:,i)))
      end do
-       
-     ! calcul valeur variables aux interfaces, k numéro interface entre i-1 et i
-     Vl(:,1) = Vh(:,1)+0.5*dV(:,1) !case "0" = copie case 1
-     Vr(:,Ncell+1) = Vh(:,Ncell)-0.5*dV(:,Ncell) !case "Ncell+1" = copie case Ncell
-     do k=1,Ncell
-        Vl(:,k+1) = Vh(:,k)+0.5*dV(:,k)
-        Vr(:,k) = Vh(:,k+1)-0.5*dV(:,k+1)
-     end do
+     
+    ! calcul flux aux interfaces
+    do k = 2,Ncell
+       F(:,k) = 0.5d0*(flux(Vh(:,k-1))+flux(Vh(:,k)))
+    end do
+    F(:,1) = F(:,2)
+    F(:,Ncell+1) = F(:,Ncell)
+    !gridFlux=F
 
-     ! calcul flux aux interfaces
-     do k = 1,Ncell+1
-        F(:,k) = 0.5*(flux(Vl(:,k))+flux(Vr(:,k)))
-     end do
-       
-     ! calcul nouvelle valeur
-     do i=1,Ncell
-        Vnew = V - (dt/dx)*(F(:,i+1)-F(:,i))
-        consVar(:,i) = Vnew
-     end do
-     t=t+dt
-   end subroutine evolTVDLF
-             
-       
-! subroutine d'evolution, v1
-  subroutine evol
-    real(kind=8),dimension(1:Nvar,1:Ncell) :: temp
-    real(kind=8) :: ctemp
-    integer :: i,j
-    ! check if condition dt<dx/c is verified
-    do i = 1,Ncell
-       ctemp = sqrt(primVar(3,i)/primVar(1,i))
-       if (ctemp>c) c=ctemp
+    ! calcul terme dissipatif
+    do l=1,Nvar
+       do k = 2,Ncell+1
+          diss(l,k) = (c+abs(primVar(2,k)))*0.5d0*(Vh(l,k-1)-Vh(l,k))
+       end do
+       diss(l,1) = diss(l,2)
+       diss(l,Ncell+1) = diss(l,Ncell)
     end do
-    if (dt > dx/c) dt=0.8*dx/c
-    if (dt < 0.8*dx/c) dt=0.8*dx/c
-    do i = 1,Ncell
-       temp(:,i) = RK4(consVar(:,i),i) 
-    end do
-    do i =1, Ncell
-       consVar(:,i)=temp(:,i)
+    
+    ! calcul nouvelle valeur
+    write(66,*) dt/dx
+    do i=1,Ncell
+       do l=1,Nvar
+          Vnew(l) = consVar(l,i) - (dt/dx)*(F(l,i+1)-F(l,i)) - (dt/dx)*(diss(l,i+1)-diss(l,i))
+          consVar(l,i) = Vnew(l)
+       end do
     end do
     t=t+dt
-  end subroutine evol
+  end subroutine evol2t1s
+  
+    
+  subroutine evol2t2s
+    integer :: i,j,k
+    real(kind=8) :: ctemp
+    real(kind=8),dimension(Nvar) :: Dm,Dp,V,Vm,Vp,Vnew
+    real(kind=8),dimension(1:Nvar,1:Ncell) :: dV,dVc,Vh
+    real(kind=8),dimension(1:Nvar,1:Ncell+1) :: F,Vl,Vr,diss
+    
+    ! vérification du pas de temps
+    do i = 1,Ncell
+       ctemp = sqrt((4.d0/3.d0)*(primVar(3,i)/primVar(1,i)))+abs(primVar(2,i))
+       if (ctemp>c) c=ctemp
+    end do
+    if (dt > 0.4*dx/c) dt=0.1*dt
+    if (dt < 0.05*dx/c) dt=10*dt
+    
+    ! calcul limiteur minmod
+    do i=1,Ncell
+       if (i==1) then
+          Vm = primVar(:,1)
+          Vp = primVar(:,2)
+       else if (i==Ncell) then
+          Vm = primVar(:,Ncell-1)
+          Vp = primVar(:,Ncell)
+       else
+          Vm = primVar(:,i-1)
+          Vp = primVar(:,i+1)
+       end if
+       Dm = primvar(:,i) - Vm
+       Dp = Vp - primVar(:,i)
+       do j=1,Nvar
+          if (Dm(j)>=0.d0 .and. Dp(j)>=0.d0) then
+             dV(j,i) = min(Dm(j),Dp(j))
+          else if (Dm(j)<=0.d0 .and. Dp(j)<=0.d0) then
+             dV(j,i) = -min(-Dm(j),-Dp(j))
+          else
+             dV(i,j) = 0.d0
+          end if
+       end do
+    end do
+    do i=1,Ncell
+       dVc(:,i) = prim2cons(dV(:,i))
+    end do
+    
+    ! calcul "demi pas de temps"
+    do i = 1,Ncell
+       Vh(:,i) = consVar(:,i) - dt/(2*dx)*(flux(consVar(:,i)+0.5*dVc(:,i))-flux(consVar(:,i)-0.5*dVc(:,i)))
+    end do
+       
+    ! calcul valeur variables aux interfaces, k numéro interface entre i-1 et i
+    do k=2,Ncell+1
+       Vl(:,k) = Vh(:,k-1)+dVc(:,k-1)
+    end do
+    Vl(:,1) = Vl(:,2)
+    do k=1,Ncell
+       Vr(:,k) = Vh(:,k)-dVc(:,k)
+    end do
+    Vr(:,Ncell+1) = Vr(:,Ncell)
+
+    ! calcul flux aux interfaces
+    do k = 1,Ncell+1
+       F(:,k) = 0.5d0*(flux(Vr(:,k))+flux(Vl(:,k)))
+    end do
+
+    ! calcul terme dissipatif
+    do k = 1,Ncell
+       diss(:,k) = (c+abs(primVar(2,k)))*0.5d0*(Vl(:,k)-Vr(:,k))
+    end do
+    diss(:,Ncell+1) = diss(:,Ncell)
+       
+    ! calcul nouvelle valeur
+    do i=1,Ncell
+       Vnew(:) = consVar(:,i) - (dt/dx)*(F(:,i+1)-F(:,i)) - (dt/dx)*(diss(:,i+1)-diss(:,i))
+       consVar(:,i) = Vnew(:)
+    end do
+    t=t+dt
+  end subroutine evol2t2s
 end program simulation
